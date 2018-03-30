867a5131c014c3bdc08a6ececbd6e923d9af6b78
tanyongkai <kaigege886@163.com>
2018-03-30 14:58:09 +0800

    update commit

--- src/main/scala/com/zhazha/spark/WordCount.scala
+++ src/main/scala/com/zhazha/spark/WordCount.scala
@@ -13,27 +13,36 @@ object WordCount extends App {
   //  lines.flatMap(line=>line.split(",")).map((_,1)).reduceByKey(_+_).foreach(println(_)) wordCount
   lines.map(line => (line.split(",")(0), line.split(",")(1), line.split(",")(2)))
     .sortBy(line => (line._1, line._3))
-    .groupBy(line => line._1)  //CompactBuffer[(String,String,String)]
+    .groupBy(line => line._1) //CompactBuffer[(String,String,String)]
     .map(line => line._2)
     .map(mapFunc)
     .sortBy(x => (x))
     .take(2)
     .foreach(println(_))
 
+  test("99")
 
-//  line =>
-//    println(line.getClass)
-//  line.foreach(l=>println(l.getClass))
-//  var tmp: String = ""
-//  line.foreach(e =>
-//    tmp = tmp + "||" + e._1 + "|" + e._2 + "|" + e._3
-//  )
-//  tmp.substring(2)
+  //  line =>
+  //    println(line.getClass)
+  //  line.foreach(l=>println(l.getClass))
+  //  var tmp: String = ""
+  //  line.foreach(e =>
+  //    tmp = tmp + "||" + e._1 + "|" + e._2 + "|" + e._3
+  //  )
+  //  tmp.substring(2)
 
-  def mapFunc(line:Iterable[(String,String,String)]):String={
-      var tmp=""
-      line.foreach(e=>tmp+="||"+e._1+"|"+e._2+"|"+e._3)
+  def mapFunc(line: Iterable[(String, String, String)]): String = {
+    var tmp = ""
+    line.foreach(e => tmp += "||" + e._1 + "|" + e._2 + "|" + e._3)
     tmp.substring(2)
   }
-  
+
+  def xx(f:String=>Int,num:String="10"):Int={
+    f(num)
+  }
+
+  def test(num:String): Unit ={
+    println(xx(num=>Integer.parseInt(num),num))
+
+  }
 }
